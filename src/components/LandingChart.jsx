55070eb6840942c14ae392b884a183a72107e87c
Daniel Nevin Richards <dnevinrichards@berkeley.edu>
2025-12-31 22:17:58 -0800

    centered email form

--- src/components/LandingChart.jsx
+++ src/components/LandingChart.jsx
@@ -17,10 +17,9 @@ export const LandingChart = props => {
     const chartContainerRef = useRef();
     const [monthlySpend, setMonthlySpend] = useState(2500)
 
-
     useEffect(
         () => {
-
+            
             const generateLineData = (props) => {
                 const {monthlySpend=2200, 
                     numYears=40, 
@@ -48,48 +47,51 @@ export const LandingChart = props => {
                 return res;
             }
 
-            const chart = createChart(chartContainerRef.current, {
-                layout: {
-                    background: { type: ColorType.Solid, color: backgroundColor },
-                    textColor,
-                },
-                width: chartContainerRef.current.clientWidth * .8,
-                height: 300,
-                grid: {
-                    vertLines: {
-                      color: 'transparent',  // Makes vertical grid lines invisible
-                      visible: false,        // Explicitly hide vertical grid lines
+            const chart = createChart(
+                chartContainerRef.current, 
+                {
+                    layout: {
+                        background: { type: ColorType.Solid, color: backgroundColor },
+                        textColor,
                     },
-                    horzLines: {
-                      color: 'transparent',  // Makes horizontal grid lines invisible
-                      visible: false,        // Explicitly hide horizontal grid lines
+                    width: chartContainerRef.current.clientWidth * .8,
+                    height: 300,
+                    grid: {
+                        vertLines: {
+                        color: 'transparent',  // Makes vertical grid lines invisible
+                        visible: false,        // Explicitly hide vertical grid lines
+                        },
+                        horzLines: {
+                        color: 'transparent',  // Makes horizontal grid lines invisible
+                        visible: false,        // Explicitly hide horizontal grid lines
+                        },
+                    }, 
+                    handleScroll: {
+                        mouseWheel: false,  // Disable scrolling with mouse wheel
+                        pressedMouseMove: false,  // Disable dragging to scroll
+                        horzTouchDrag: false,  // Disable horizontal touch scroll
+                        vertTouchDrag: false,  // Disable vertical touch scroll
                     },
-                }, 
-                handleScroll: {
-                    mouseWheel: false,  // Disable scrolling with mouse wheel
-                    pressedMouseMove: false,  // Disable dragging to scroll
-                    horzTouchDrag: false,  // Disable horizontal touch scroll
-                    vertTouchDrag: false,  // Disable vertical touch scroll
-                },
-                handleScale: {
-                    axisPressedMouseMove: false,  // Disable scaling by dragging axis
-                    mouseWheel: false,  // Disable zooming with mouse wheel
-                    pinch: false,  // Disable pinch-to-zoom for touch devices
-                },
-                timeScale: {
-                    // Custom time scale options
-                    timeVisible: true,                // Makes sure the time is visible
-                    secondsVisible: false,             // Hides seconds (useful if the chart updates frequently)
-                    tickMarkFormatter: (time, tickMarkType, locale) => {
-                      // Custom formatter for x-axis ticks
-                      const date = new Date(time); // Convert to milliseconds
-                      return `${date.getMonth()}-${date.getFullYear()}`;
+                    handleScale: {
+                        axisPressedMouseMove: false,  // Disable scaling by dragging axis
+                        mouseWheel: false,  // Disable zooming with mouse wheel
+                        pinch: false,  // Disable pinch-to-zoom for touch devices
                     },
-                  },
-                  rightPriceScale: {
-                    borderVisible: false,
-                  },
-            });
+                    timeScale: {
+                        // Custom time scale options
+                        timeVisible: true,                // Makes sure the time is visible
+                        secondsVisible: false,             // Hides seconds (useful if the chart updates frequently)
+                        tickMarkFormatter: (time, tickMarkType, locale) => {
+                        // Custom formatter for x-axis ticks
+                        const date = new Date(time); // Convert to milliseconds
+                        return `${date.getMonth()}-${date.getFullYear()}`;
+                        },
+                    },
+                    rightPriceScale: {
+                        borderVisible: false,
+                    },
+                }
+            );
             chart.timeScale().fitContent();
 
             const exponentialSeriesProps = {'monthlySpend':monthlySpend, 'numYears':40, 'cashbackRate':0.045, 'growthRate':1.12}
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+
 
 const CustomSlider = ({props}) => {
     let { min, max, step, initialValue, sliderHandleChange } = props;
